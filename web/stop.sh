5cfd06eeadbda11cfa814ce4ba2ea53478303058
Peter Schäfer <pete.schaefer@gmail.com>
2025-05-07 14:00:53 +0200

    small fish

--- web/stop.sh
+++ web/stop.sh
@@ -2,19 +2,11 @@
 # $1 path to jose installation
 # $2 path to Tomcat installation
 
-# call with:
-#  source setenv.sh
-
-export jose_workdir=$1
-export jose_db=MySQL-standalone
-export jose_db_port=3306
-export jose_splash=off
-
 export JAVA_HOME=$1/jre
 export JRE_HOME=$1/jre
 
 export CATALINA_HOME=$2
 export CATALINA_BASE=$2
 
-
+$CATALINA_HOME/bin/catalina.sh stop
 
